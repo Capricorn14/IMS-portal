8fca9e4e0703d6a5f2d497a0a9dd8f33423bd83e
Asavari S Bhelawe <basavari2003@gmail.com>
2023-03-13 23:05:59 +0530

    ims code

---
+++ uba/dashboard.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+  <head>    
+    <link rel="stylesheet" href="../styles.css">
+  </head>
+  <body> 
+
+   <div class="main">
+    <div class="container">
+    <div class="card">
+      <div class="box">
+        <div class="content">
+          <h4>WORSKSHOP/
+            SEMINARS CONDUCTED
+          </h4>
+          <p>organised by the Club</p>
+          <a href="fdp-sttp/index.php">Go to Criteria</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="box">
+        <div class="content">
+          <h4>OUTREACH PROGRAMME</h4>
+          <p>organised by the Club</p>
+          <a href="book-chapters/index.php">Go to Criteria</a>
+        </div>
+      </div>
+    </div>
+
+        
+  <?php include('../header.php'); ?>  
+  </body>
+</html>
+
